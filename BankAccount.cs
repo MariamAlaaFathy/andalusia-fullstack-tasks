bd8cdecfa17aaac6f4a0c4ee07b98aa3329fc66c
MariamAlaaFathy <mariam.alaafathy@gmail.com>
2026-08-06 16:44:12 +0300

    Task One Done

---
+++ BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskOne
+{
+    internal class BankAccount
+    {
+        private decimal _balance;
+        public decimal Balance { get { return _balance; } }
+        public string Owner { get; }
+
+        public BankAccount(string owner, decimal initialBalance)
+        {
+            Owner = owner;
+            _balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0) {
+                Console.WriteLine("Deposit amount cannot be negative.");
+                
+            } else {
+                _balance += amount;
+                Console.WriteLine($"{amount} was deposited in your account. Total balance: {_balance}");
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 0) {
+                Console.WriteLine("Withdrawal amount cannot be negative.");
+            } else if (amount > _balance) {
+                Console.WriteLine("Insufficient funds.");
+            } else {
+                _balance -= amount;
+                Console.WriteLine($"{amount} was withdrawn from your account. Total balance: {_balance}");
+            }
+        }
+
+        public virtual string GetAccountType()
+        {
+            return "Standard";
+        }
+    }
+}
