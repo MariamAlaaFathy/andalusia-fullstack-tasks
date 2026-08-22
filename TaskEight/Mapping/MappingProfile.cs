2a94a7bb278c298976ca4ec1aeaf6715422380ff
MariamAlaaFathy <mariam.alaafathy@gmail.com>
2026-08-22 06:41:40 +0300

    Task Eight

--- TaskEight/Mapping/MappingProfile.cs
+++ TaskEight/Mapping/MappingProfile.cs
@@ -7,8 +7,58 @@ namespace TaskEight.Mapping
     public class MappingProfile : Profile
     {
         public MappingProfile() {
-            CreateMap<Tasks, TasksDTO>();
+
+            // Tasks => TasksDTO
+            CreateMap<Tasks, TasksDTO>()
+            .ForMember(
+                dest => dest.UserName, 
+                opt => opt.MapFrom(src => src.User != null ? src.User.Name : null)
+            );
+
+            // Tasks => TaskSummaryDTO
             CreateMap<Tasks, TaskSummaryDTO>();
+
+            // CreateTaskRequest => Tasks
+            CreateMap<CreateTaskRequest, Tasks>()
+            .ForMember(
+                dest => dest.Id,
+                opt => opt.Ignore()
+            )
+            .ForMember(
+                dest => dest.IsCompleted, 
+                opt => opt.MapFrom(_ => false)
+            )
+            .ForMember(
+                dest => dest.CreatedAt, 
+                opt => opt.MapFrom(_ => DateTime.UtcNow)
+            )
+            .ForMember(
+                dest => dest.UpdatedAt, 
+                opt => opt.Ignore()
+            )
+            .ForMember(
+                dest => dest.User, 
+                opt => opt.Ignore()
+            );
+
+            // UpdateTaskRequest => Tasks
+            CreateMap<UpdateTaskRequest, Tasks>()
+            .ForMember(
+                dest => dest.Id, 
+                opt => opt.Ignore()
+            )
+            .ForMember(
+                dest => dest.CreatedAt, 
+                opt => opt.Ignore()
+            )
+            .ForMember(
+                dest => dest.UpdatedAt, 
+                opt => opt.Ignore()
+            )
+            .ForMember(
+                dest => dest.User, 
+                opt => opt.Ignore()
+            );
         }
     }
 }
